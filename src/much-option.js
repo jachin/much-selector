0ec307ad99a7ecfabb06a0bee0c4325e6c795c58
Jachin Rupe <jachin.rupe@gmail.com>
2020-07-08 01:23:09 -0500

    parsing options out of a select element

---
+++ src/much-option.js
@@ -0,0 +1,48 @@
+class MuchOption {
+  constructor(value) {
+    this._value = value;
+    this._lable = value;
+    this._selected = false;
+    this._index = null;
+  }
+
+  set value(value) {
+    this._value = value;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  set label(label) {
+    this._label = label;
+  }
+
+  get label() {
+    return this._label;
+  }
+
+  set selected(selected) {
+    if (typeof selected !== "boolean") {
+      throw "MuchOption index must be a boolean";
+    }
+    this._selected = selected;
+  }
+
+  get selected() {
+    return this._selected;
+  }
+
+  set index(index) {
+    if (typeof index !== "number") {
+      throw "MuchOption index must be a number";
+    }
+    this._index = index;
+  }
+
+  get index() {
+    return this._index;
+  }
+}
+
+export {MuchOption};
--- src/much-selector.js
+++ src/much-selector.js
@@ -1,13 +1,32 @@
 import {LitElement, html, css} from 'lit-element';
 
+import {MuchOption} from "./much-option.js";
+
+const buildOptionsFromSelecteElement = selectElement => {
+  const options = new Map();
+  const optionElements = selectElement.querySelectorAll("option");
+  optionElements.forEach((optionElement, optionIndex) => {
+    const option = new MuchOption(optionElement.getAttribute("value"));
+    option.label = optionElement.innerText;
+    option.index = optionIndex;
+    if (optionElement.hasAttribute("selected")) {
+      const optionSelectedValue = optionElement.getAttribute("selected");
+      if (optionSelectedValue === "false") {
+        option.selected = false;
+      } else {
+        option.selected = true;
+      }
+    }
+    options.set(option.value, option);
+  });
+  return options;
+}
+
 class MuchSelector extends LitElement {
   static get styles() {
     return css`
-      :host {
-        display: block;
-        border: solid 1px gray;
-        padding: 16px;
-        max-width: 800px;
+      slot {
+        display: none;
       }
     `;
   }
@@ -15,14 +34,22 @@ class MuchSelector extends LitElement {
   static get properties() {
     return {
       options: {type: Map},
-      selectedOptions: {type: Map},
     };
   }
 
   constructor() {
     super();
     this.options = new Map();
-    this.selectedOptions = new Map();
+  }
+
+  firstUpdated() {
+    const selectElements = this.querySelectorAll("select");
+    if (selectElements.length > 1) {
+      throw "The much-selector element only allows one select element in it's slot."
+    }
+    selectElements.forEach(selectElement => {
+      this.options = buildOptionsFromSelecteElement(selectElement);
+    });
   }
 
   render() {
